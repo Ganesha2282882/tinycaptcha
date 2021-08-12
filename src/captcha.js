be44a3b9e88a815d936f60fe954d2d5921f0ade7
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-08-12 15:03:40 -0400

    Update captcha.js

--- src/captcha.js
+++ src/captcha.js
@@ -1,6 +1,6 @@
 var box = document.getElementById("tc-captcha");
 let number = Math.floor(Math.random() * 1000);
-box.innerHTML = `<p>Please solve the CAPTCHA:</p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${number}"><br><input id="number"><br><textarea name="tcr" id="tcr" style="display: none;">`;
+box.innerHTML = `<p>Please solve the CAPTCHA:</p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${number}"><br><input id="number"><br><textarea name="tcr" id="tcr" style="display: none;">fail</textarea>`;
 document.getElementById("number").addEventListener("change", (e) => {
     if (e.target.value == number) {
         number = null;
